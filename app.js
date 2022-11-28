f0b7c0ef445db97e090c517d6c4d54bcc293f0c3
Adam Bolton <adam_bolton90@hotmail.com>
2022-11-28 15:49:28 +0900

    backend test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const api_key = process.env.API_KEY;
 //   res.send("Hello World!");
 // });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 
