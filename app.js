848c250ee794ea5a26817d087531bb5a5ba259d6
Adam Bolton <adam_bolton90@hotmail.com>
2022-11-28 15:44:15 +0900

    backend test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const api_key = process.env.API_KEY;
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
