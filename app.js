5841d89e146eaa7db1e3f008e6b15c864da8cd6d
Adam Bolton <adam_bolton90@hotmail.com>
2022-12-13 12:57:24 +0900

    backend test

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 5000;
 
 const api_key = process.env.API_KEY;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 
 // app.get('/', (req, res) => {
