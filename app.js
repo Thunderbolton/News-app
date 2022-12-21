a0b987c239696bd890e51e3c3091e2384a5c1f8b
Adam Bolton <adam_bolton90@hotmail.com>
2022-12-22 01:17:48 +0900

    updated app.js

--- app.js
+++ app.js
@@ -159,11 +159,11 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "../client/build")))
+  app.use(express.static(path.join(__dirname, '../client/build')))
 
   app.get('*', (req, res) => {
   
-  res.sendFile(path.join(__dirname, "../client'/'build'/'index.html"))
+  res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'))
 })
 } else {
   app.get('/', (req, res) => res.send('Set to production'))
