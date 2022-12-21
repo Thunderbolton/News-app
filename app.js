58217c7f9fae0532ce0f669665cedffa8199f8ea
Adam Bolton <adam_bolton90@hotmail.com>
2022-12-21 15:09:00 +0900

    updated app.js

--- app.js
+++ app.js
@@ -159,11 +159,11 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/public')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   app.get('*', (req, res) => {
   
-  res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 }
 
